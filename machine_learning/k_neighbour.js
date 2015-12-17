268e68155a69e832cf517f896ce6a0c8cfba0477
Julius Rajala <juliusrajala@gmail.com>
2015-12-17 20:50:32 +0200

    Finished nearest-neighbor algorithm, created simple html document for testing purposes.

--- machine_learning/k_neighbour.js
+++ machine_learning/k_neighbour.js
@@ -10,7 +10,7 @@ var NodeList = function(k){
 }
 
 NodeList.prototype.add = function(node){
-  this.nodes.add(node);
+  this.nodes.push(node);
 }
 
 //Finds minimum and maximum values for the rooms and areas.
@@ -36,13 +36,150 @@ NodeList.prototype.calculateRanges = function(){
       this.areas.max = this.nodes[i].area;
     }
   }
-}
+};
 
-function populateNodeList(data){
+NodeList.prototype.determineUnknown = function(){
+  this.calculateRanges();
+
+  for(var i in this.nodes){
+    if(!this.nodes[i].type){
+      //Clone the nodes if the node is an unknown type.
+      this.nodes[i].neighbors = [];
+      for(var j in this.nodes){
+        if(!this.nodes[j].type){
+          continue;
+        }
+        this.nodes[i].neighbors.push(new Node(this.nodes[j]));
+      }
+
+      //Measure distance, sort by distance and guess the type
+      this.nodes[i].measureDistances(this.areas, this.rooms);
+      this.nodes[i].sortByDistance();
+      console.log(this.nodes[i].guessType(this.k));
+    }
+  }
+};
+
+Node.prototype.measureDistances = function(area_range_obj, rooms_range_obj){
+  var rooms_range = rooms_range_obj.max - rooms_range_obj.min;
+  var area_range = area_range_obj.max - area_range_obj.min;
+
+  for(var i in this.neighbors){
+    var neighbor = this.neighbors[i];
+
+    var delta_rooms = neighbor.rooms - this.rooms;
+    delta_rooms = (delta_rooms)/rooms_range;
+
+    var delta_area = neighbor.area - this.area;
+    delta_area = (delta_area)/area_range;
+
+    neighbor.distance = Math.sqrt(delta_rooms*delta_rooms + delta_area*delta_area);
+  }
+};
+
+Node.prototype.sortByDistance = function(){
+  this.neighbors.sort(function(a,b){
+    return a.distance - b.distance;
+  });
+};
+
+Node.prototype.guessType = function(k){
+  var types = {};
+
+  for(var i in this.neighbors.slice(0,k)){
+    var neighbor = this.neighbors[i];
+
+    if(!types[neighbor.type]){
+      types[neighbor.type] = 0;
+    }
+    types[neighbor.type] += 1;
+  }
+
+  var guess = {type: false, count: 0};
+  for(var type in types){
+    if(types[type] > guess.count){
+      guess.type = type;
+      guess.count = types[type];
+    }
+  }
+  this.guess = guess;
+
+  return types;
+};
+
+NodeList.prototype.draw = function(canvas_id){
+  var rooms_range = this.rooms.max - this.rooms.min;
+  var areas_range = this.areas.max - this.areas.min;
+
+  var canvas = document.getElementById(canvas_id);
+  var ctx = canvas.getContext("2d");
+  var width = 400;
+  var height = 400;
+  ctx.clearRect(0,0,width,height);
+
+  for(var i in this.nodes){
+    ctx.save();
+
+    switch(this.nodes[i].type){
+      case 'apartment':
+        ctx.fillStyle = 'red';
+        break;
+      case 'house':
+        ctx.fillStyle = 'green';
+        break;
+      case 'flat':
+        ctx.fillStyle = 'blue';
+        break;
+      default:
+        ctx.fillStyle = '#666666';
+    }
+    var padding = 40;
+    var x_shift_pct = (width - padding)/width;
+    var y_shift_pct = (height - padding)/height;
+
+    var x = (this.nodes[i].rooms - this.rooms.min)* (width/rooms_range)*x_shift_pct + (padding/2);
+    var y = (this.nodes[i].rooms - this.rooms.min)* (height/rooms_range)*y_shift_pct + (padding/2);
+    y = Math.abs(y - height);
+
+    ctx.translate(x,y);
+    ctx.beginPath();
+    ctx.arc(0,0,5,0,Math.PI*2, true);
+    ctx.fill();
+    ctx.closePath();
+
+    if(!this.nodes[i].type){
+      switch(this.nodes[i].guess.type){
+        case 'apartment':
+          ctx.strokeStyle = 'red';
+          break;
+        case 'house':
+          ctx.strokeStyle = 'green';
+          break;
+        case 'flat':
+          ctx.strokeStyle = 'blue';
+          break;
+        default:
+          ctx.strokeStyle = '#666666';
+      }
+
+      var radius = this.nodes[i].neighbors[this.k - 1].distance * width;
+
+      radius *=x_shift_pct;
+      ctx.beginPath();
+      ctx.arc(0,0,radius,0,Math.PI*2, true);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    ctx.restore();
+  }
+};
+
+/*function populateNodeList(data){
   for(key in data){
     NodeList.add(Node(data[key]));
   }
-}
+}*/
 
 var data = [
     {rooms: 1, area: 350, type: 'apartment'},
@@ -72,4 +209,22 @@ var data = [
     {rooms: 1, area: 1300, type: 'flat'},
 ];
 
-populateNodeList(data);
+var run = function(){
+  nodes = new NodeList(3);
+  for(var i in data){
+    nodes.add(new Node(data[i]));
+  }
+  var random_rooms = Math.round(Math.random() * 10);
+  var random_area = Math.round(Math.random() * 2000);
+  nodes.add( new Node({rooms: random_rooms, area: random_area, type: false}));
+
+  nodes.determineUnknown();
+  nodes.draw("canvas");
+};
+
+window.onload = function(){
+  setInterval(run, 5000);
+  run();
+}
+
+// populateNodeList(data);

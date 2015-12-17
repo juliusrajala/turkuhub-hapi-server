d801d719bafcb770a3448635c4888f9f7f62ff11
Julius Rajala <juliusrajala@gmail.com>
2015-12-17 19:38:04 +0200

    More work on locating nearest-neighbour.

--- machine_learning/k_neighbour.js
+++ machine_learning/k_neighbour.js
@@ -7,8 +7,34 @@ var Node = function(object){
 var NodeList = function(k){
   this.nodes = [];
   this.k = k;
-  this.add = function(node){
-    this.nodes.add(node);
+}
+
+NodeList.prototype.add = function(node){
+  this.nodes.add(node);
+}
+
+//Finds minimum and maximum values for the rooms and areas.
+NodeList.prototype.calculateRanges = function(){
+  this.areas = {min: 1000000, max: 0};
+  this.rooms = {min: 1000000, max: 0};
+  for(var i in this.nodes){
+    //If the rooms on this node are less than the room min
+    //sets the smaller node as the minimum
+    if(this.nodes[i].rooms < this.rooms.min){
+      this.rooms.min = this.nodes[i].rooms;
+    }
+    //Does the same as above but seeks for the max
+    if(this.nodes[i].rooms > this.rooms.max){
+      this.rooms.max = this.nodes[i].rooms;
+    }
+    //Same as the first if-structure for the areas
+    if(this.nodes[i].area < this.areas.min){
+      this.areas.min = this.nodes[i].area;
+    }
+    //Follows earlier structure.
+    if(this.nodes[i].area > this.areas.max){
+      this.areas.max = this.nodes[i].area;
+    }
   }
 }
 
